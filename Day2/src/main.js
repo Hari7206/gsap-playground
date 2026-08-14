4c40c420d9e26d2a0a0a9e41ad51b9153854e329
Hari Thapa <harithapa4654@gmail.com>
2026-08-14 16:41:30 +0530

    animation

--- Day2/src/main.js
+++ Day2/src/main.js
@@ -1,13 +1,26 @@
-import gsap from 'gsap'
-import './style.css'
+import gsap from "gsap";
+import "./style.css";
 
+document.querySelectorAll(".marquee").forEach((marquee, index) => {
+  const track = marquee.querySelector(".marquee-track");
+  const content = track.querySelector(".marquee-text");
 
+  track.appendChild(content.cloneNode(true));
 
-gsap.to(".box", {
-  x: 500,
-  y: 200,
-  duration: 1,
-  repeat: -1,
-  yoyo: true,
-  ease: "bounce.inOut"
+  const duration = index === 0 ? 12 : 18;
+
+  const animation = gsap.to(track.querySelectorAll(".marquee-text"), {
+    xPercent: -100,
+    duration: duration,
+    repeat: -1,
+    ease: "none",
+  });
+
+  marquee.addEventListener("mouseenter", () => {
+    animation.pause();
+  });
+
+  marquee.addEventListener("mouseleave", () => {
+    animation.play();
+  });
 });

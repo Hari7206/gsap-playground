c81cd19bf12d69ebfc3e24819f39d2146d94db02
Hari Thapa <harithapa4654@gmail.com>
2026-08-22 00:08:28 +0530

    day3b

--- day3b/src/main.js
+++ day3b/src/main.js
@@ -2,6 +2,18 @@ import gsap from "gsap";
 import "./style.css";
 
 // timeline 
+
+//  we have differnt types for setting the timeline 
+//  < same time of upper element 
+//  -=0.2  so its means it is subtract to this much of time then wding time 
+//  += this will work as delay
+
+
+
+
+
+//  lables -> so here we can give name to 2 same name to the element and if they both have same name then they will eventually work together
+
 const tl = gsap.timeline();
 
 
@@ -14,7 +26,9 @@ tl.to('.box', {
   x: 1200,
   duration: 1.3,
   ease: "power4.out",
-}).to('.box2', {
+} ,
+"<"   //this means it make the first and this same timing 
+).to('.box2', {
   x: 1200,
   duration: 1.3,
   ease: "power4.out",
@@ -22,6 +36,8 @@ tl.to('.box', {
   x: 1200,
   duration: 1.3,
   ease: "power4.out",
-})
+} ,
+"< 0.1"   // this means with its time add this 0.1 time also 
+)
 
 
